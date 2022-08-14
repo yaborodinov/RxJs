0ce45321fb835fe291610153a2e4be966170d366
Alexandr Borodinov <yaborodinov@gmail.com>
2022-08-14 23:28:38 +0300

    array mapping, compare native js vs RxJs

--- src/problem.js
+++ src/problem.js
@@ -1,3 +1,5 @@
+import {interval} from 'rxjs'
+import {filter, map, scan, take} from 'rxjs/operators'
 const btn = document.getElementById('interval')
 const rxjsBtn = document.getElementById('rxjs')
 const display = document.querySelector('#problem .result')
@@ -11,3 +13,43 @@ const people = [
   {name: 'Irina', age: 23},
   {name: 'Oleg', age: 20}
 ]
+
+
+btn.addEventListener('click', () => {
+  btn.disabled = true;
+  let i = 0;
+  const canDrink = []
+
+  const interval = setInterval(() => {
+
+    if (people[i]){
+      if (people[i].age >= 18) {
+        canDrink.push(people[i].name) 
+      }
+
+      display.textContent = canDrink.join(' ');
+      i++;
+
+    } else {
+      clearInterval(interval)
+      btn.disabled = false;
+    }
+
+  }, 1000)
+})
+
+
+rxjsBtn.addEventListener('click', () => {
+  rxjsBtn.disabled = true;
+
+  interval(1000)
+  .pipe(
+    take(people.length),
+    filter(value => people[value].age >= 18 ),
+    map(value => people[value].name),
+    scan((acc, value) => acc.concat(value), [])
+  )
+  .subscribe(res => {
+    display.textContent = res.join('  ')
+  }, null, () => rxjsBtn.disabled = false)
+})
